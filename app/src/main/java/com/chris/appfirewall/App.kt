46bb1c7e27991471ebc7811ad544fba5c73bad69
Demons15 <Yq8251341>
2019-09-19 16:50:28 +0800

    ....android 防火墙,需要root

--- app/src/main/java/com/chris/appfirewall/App.kt
+++ app/src/main/java/com/chris/appfirewall/App.kt
@@ -1,15 +1,30 @@
 package com.chris.appfirewall
 
 import android.app.Application
+import android.database.sqlite.SQLiteCantOpenDatabaseException
+import com.cxsz.framework.tool.LogUtil
 import com.cxsz.framework.tool.SystemManager
+import com.raizlabs.android.dbflow.config.FlowConfig
+import com.raizlabs.android.dbflow.config.FlowManager
 import dev.ukanth.ufirewall.util.AppUtils
 
 class App:Application(){
+    val TAG = "AFWall"
     override fun onCreate() {
         super.onCreate()
         val apkRoot = "chmod 777 $packageCodePath"
         SystemManager.RootCommand(apkRoot)
+        try {
+            FlowManager.init(
+                FlowConfig.Builder(this)
+                    .openDatabasesOnInit(true).build()
+            )
+        } catch (e: SQLiteCantOpenDatabaseException) {
+            LogUtil.setTagI(TAG, "unable to open database - exception")
+        }
 
+        AppUtils.getInstance().init(this)
+        AppUtils.getInstance().reloadPrefs()
         AppUtils.getInstance().init(this)
         AppUtils.getInstance().reloadPrefs()
     }
